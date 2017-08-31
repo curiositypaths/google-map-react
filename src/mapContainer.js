fc6f654aff3bb91fc73645162d85b255254aa445
curiositypaths <curiositypaths@users.noreply.github.com>
2017-08-31 14:32:14 -0400

    Add marker

--- src/mapContainer.js
+++ src/mapContainer.js
@@ -2,8 +2,17 @@ import React, { Component } from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 export class MapContainer extends React.Component {
-  mapClicked = (eee) => {
-    debugger
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  mapClicked = (mapProps, map, clickEvent) => {
+
+    this.setState({children:<Marker
+title={'The marker`s title will appear as a tooltip.'}
+name={'SOMA'}
+position={{lat: clickEvent.latLng.lat(), lng:clickEvent.latLng.lng()}} />})
   }
 
   /*
@@ -21,10 +30,14 @@ export class MapContainer extends React.Component {
   render() {
     return (
       <Map initialCenter={{
-            lat: 37.774929,
-            lng:  -122.419416
+            lat: 37.81873440498779,
+            lng:  -122.18075752258301
           }} onClick={this.mapClicked} google={this.props.google} zoom={14}>
-    
+
+          {this.state.children}
+
+
+
 
       </Map>
     )
